4e0f38798f1ec96353c8895439d045b9644a7685
TheAwiteb <awiteb@hotmail.com>
2022-05-13 20:31:31 +0300

    ✨: Add notification template

--- src/main.py
+++ src/main.py
@@ -39,41 +39,92 @@ CONFIG = Config("lasts.json")
 BIBLIOGRAM_NITTER = BibliogramNitter(
     DOTENV.get("NOTIFICATION_BOT_TWITTER_USERNAME", None),
     DOTENV.get("NOTIFICATION_BOT_INSTAGRAM_USERNAME", None),
-    DOTENV.get("NOTIFICATION_BOT_NAME", None),
+    DOTENV.get("NOTIFICATION_BOT_NAME", ""),
 )
 BOT = TeleBot(DOTENV.get("NOTIFICATION_BOT_TOKEN", None))
+
+
+def notify_format(vars: dict, service: Services) -> str:
+    """ Format notification
+
+    Args:
+        vars (dict): Notification variables
+        service (Services): Notification service
+
+    Raises:
+        Exception: If the service unsupported
+        TypeError: If service type not `Services`
+
+    Returns:
+        str: Notification context
+    """
+    if isinstance(service, Services):
+        if service == Services.Twitter:
+            template = DOTENV.get(
+                "NOTIFICATION_BOT_TWITTER_TEMPLATE",
+                "{TWEET_DESCRIPTION}\n\n> {TWITTER_LINK}",
+            )
+        elif service == Services.Instagram:
+            template = DOTENV.get(
+                "NOTIFICATION_BOT_INSTAGRAM_TEMPLATE",
+                "{POST_DESCRIPTION}\n\n> {INSTAGRAM_LINK}",
+            )
+        else:
+            raise Exception(f"`{service.name}` unsupported service")
+    else:
+        raise TypeError(
+            f"`{service.__class__.__name__}` is invalid service type, should be `Services`"
+        )
+    for var, value in vars.items():
+        template = template.replace(f"{{{var}}}", value)
+    return template
 
 
 def send_notification() -> None:
     last_tweet = BIBLIOGRAM_NITTER.last_nitter_post()
     last_post = BIBLIOGRAM_NITTER.last_bibliogram_post()
-    chat_id = DOTENV.get("NOTIFICATION_BOT_TELEGRAM_CHANNEL_ID", None)
+    chat_id = DOTENV.get("NOTIFICATION_BOT_TELEGRAM_CHAT_ID", None)
 
-    if last_tweet and last_tweet != CONFIG.tweet:
-        CONFIG.tweet = last_tweet
-        text = f"تغريدة جديدة من {BIBLIOGRAM_NITTER.name}:\n{last_tweet.description}\n\n> {last_tweet.twitter_url.twitter}"
-        if last_tweet.medias:
-            if "video" in last_tweet.medias[0]:
-                BOT.send_video(chat_id, last_tweet.medias[0], caption=text)
+    if chat_id:
+        if last_tweet and last_tweet != CONFIG.tweet:
+            CONFIG.tweet = last_tweet
+            tweet_variables = {
+                "CHAT_ID": chat_id,
+                "TWITTER_LINK": last_tweet.twitter_url.twitter,
+                "NITTER_LINK": last_tweet.twitter_url.nitter,
+                "TWEET_DESCRIPTION": last_tweet.description,
+            }
+            text = notify_format(tweet_variables, Services.Twitter)
+            if last_tweet.medias:
+                if "video" in last_tweet.medias[0]:
+                    BOT.send_video(chat_id, last_tweet.medias[0], caption=text)
+                else:
+                    BOT.send_photo(chat_id, last_tweet.medias[0], caption=text)
             else:
-                BOT.send_photo(chat_id, last_tweet.medias[0], caption=text)
-        else:
-            BOT.send_message(chat_id, text)
+                BOT.send_message(chat_id, text)
 
-    if last_post != CONFIG.post:
-        CONFIG.post = last_post
-        text = f"منشور جديد من {BIBLIOGRAM_NITTER.name}:\n{last_post.description}\n\n> {last_post.insta_url.instagram}"
-        if last_post.medias:
-            if "video" in last_post.medias[0]:
-                BOT.send_video(chat_id, last_post.medias[0], caption=text)
+        if last_post and last_post != CONFIG.post:
+            CONFIG.post = last_post
+            post_variables = {
+                "CHAT_ID": chat_id,
+                "INSTAGRAM_LINK": last_post.insta_url.instagram,
+                "BIBLIOGRAM_LINK": last_post.insta_url.bibliogram,
+                "POST_DESCRIPTION": last_post.description,
+            }
+            text = notify_format(post_variables, Services.Instagram)
+            if last_post.medias:
+                if "video" in last_post.medias[0]:
+                    BOT.send_video(chat_id, last_post.medias[0], caption=text)
+                else:
+                    BOT.send_photo(chat_id, last_post.medias[0], caption=text)
             else:
-                BOT.send_photo(chat_id, last_post.medias[0], caption=text)
-        else:
-            BOT.send_message(chat_id, text)
+                BOT.send_message(chat_id, text)
+    else:
+        raise Exception("There is no chat_id")
 
 
 def main() -> None:
-    seconds = int(DOTENV.get("NOTIFICATION_BOT_DELAY", None))
+    seconds = int(DOTENV.get("NOTIFICATION_BOT_DELAY", 120))
     while True:
         try:
             send_notification()
